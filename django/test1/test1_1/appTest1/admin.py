d53f862ff540a6d15d603be3d67142575ea75c15
Duke Togo <golgo8128@yahoo.co.jp>
2023-03-31 16:05:02 +0900

    ...

---
+++ django/test1/test1_1/appTest1/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import TestTable1
+
+admin.site.register(TestTable1)
+
---
+++ django/test1/test1_1/appTest1/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+
+from django.contrib.auth.models import User
+
+class TestTable1(models.Model):
+
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE)
+
